c0bd9f4d9ac4d313c7b5f768828684653134dc97
William Hammond <william.t.hammond@gmail.com>
2022-10-11 14:52:54 -0400

    Adds resume to copy through

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
     eleventyConfig.addPassthroughCopy("./src/style.css");
+    eleventyConfig.addPassthroughCopy("./src/resume/william-hammond-resume.pdf");
     return {
         dir: {
             input: "src",
