ab5094635c66aad76b3b1921e4b4da0f47003f3b
William Hammond <william.t.hammond@gmail.com>
2022-10-11 12:04:24 -0400

    Adds basic nav bar

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,14 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./src/style.css")
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
+    eleventyConfig.addPassthroughCopy("./src/style.css");
     return {
         dir: {
             input: "src",
             output: "_site",
+            includes: "_includes",
+            data: "_data",
         },
     };
 };
--- src/index.md
+++ src/index.md
@@ -1,6 +1,9 @@
 ---
 title: Hello World
 layout: "base.njk"
+eleventyNavigation:
+  key: Home
 ---
 
+# William Hammond
 Hello test!
